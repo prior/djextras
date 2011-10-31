ef1d28e98ffd3ca02d8475c44130f064db1d0635
prior <prior@cracklabs.com>
2011-10-31 12:12:51 -0400

    bump requirment versions

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from distutils.core import setup
 
 setup(
     name='djextras',
-    version='1.0.1',
+    version='1.0.2',
     description="Some extra django goodies",
     long_description = open('README.md').read(),
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/djextras',
-    download_url='https://github.com/prior/djextras/tarball/v1.0.1',
+    download_url='https://github.com/prior/djextras/tarball/v1.0.2',
     license='LICENSE.txt',
     packages=['djextras'],
     install_requires=[
         'nose==1.1.2',
         'unittest2==0.5.1',
-        'Django==1.3.1',
-        'sanetime==3.0.0'
+        'Django==1.3',
+        'sanetime==3.0.1'
     ],
-    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.0.0'],
+    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.0.1'],
 )
